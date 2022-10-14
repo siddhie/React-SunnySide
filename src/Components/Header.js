fa544963fd1df97a30fb8b5ff298527350fa692d
Siddhi Nandaniya <siddhinandaniya2811@gmail.com>
2022-10-14 23:45:41 +0530

    header and navbar done

---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <h1 class="main__heading">We are creatives</h1>
+      <img
+        src="images/desktop/image-header.jpg"
+        alt="header_backgroundImage"
+        className="header__image"
+      />
+    </div>
+  );
+}
