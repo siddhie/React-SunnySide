bb2befa4b9d8801830ceff646f6086d4c67707ca
Siddhi Nandaniya <siddhinandaniya2811@gmail.com>
2022-10-19 21:47:44 +0530

    last changes

--- src/Components/Fourimages.js
+++ src/Components/Fourimages.js
@@ -4,50 +4,50 @@ import "./Fourimages.css";
 export default function Fourimages() {
   return (
     <section className="four__images">
-      <div className="img__1">
+      <div className=" img_div">
         <img
-          className="four_img image__pc"
+          className="four_img image__pc  "
           src="images/desktop/image-gallery-milkbottles.jpg"
           alt=""
         />
         <img
-          className="four_img image__mobile"
+          className="four_img image__mobile   "
           src="images/mobile/image-gallery-milkbottles.jpg"
           alt=""
         />
       </div>
-      <div className="img__2">
+      <div className=" img_div ">
         <img
           className="four_img image__pc"
           src="images/desktop/image-gallery-orange.jpg"
           alt=""
         />
         <img
-          className="four_img image__mobile"
+          className="four_img image__mobile  "
           src="images/mobile/image-gallery-orange.jpg"
           alt=""
         />
       </div>
-      <div className="img__3">
+      <div className=" img_div">
         <img
           className="four_img image__pc"
           src="images/desktop/image-gallery-cone.jpg"
           alt=""
         />
         <img
-          className="four_img image__mobile"
+          className="four_img image__mobile  "
           src="images/mobile/image-gallery-cone.jpg"
           alt=""
         />
       </div>
-      <div className="img__4">
+      <div className=" img_div">
         <img
-          className="four_img image__pc"
+          className="four_img image__pc  "
           src="images/desktop/image-gallery-sugarcubes.jpg"
           alt=""
         />
         <img
-          className="four_img image__mobile"
+          className="four_img image__mobile "
           src="images/mobile/image-gallery-sugar-cubes.jpg"
           alt=""
         />
